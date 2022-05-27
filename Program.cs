eb35516f3086c11b01f6f7385770902787ab71a3
Gabriel Ferreira <gabrielcarfepro@gmail.com>
2022-05-27 12:07:43 -0300

    Added the main cs file to project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using jogoRPG.src.Entities;
+
+namespace jogoRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero hero = new Hero("Arus", 23, "Knight");
+            Wizard wizard = new Wizard("Jennica", 23, "Wizard");
+
+            Console.WriteLine(wizard.Attack());
+        }
+    }
+}
