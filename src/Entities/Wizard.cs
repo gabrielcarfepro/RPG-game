fee4f3e1847514467e608aec4d831ff84a9a96d1
Gabriel Ferreira <gabrielcarfepro@gmail.com>
2022-05-29 14:02:52 -0300

    Included the new character attribute, named life. Changed the parameters of Attack method

--- src/Entities/Wizard.cs
+++ src/Entities/Wizard.cs
@@ -3,14 +3,26 @@ namespace jogoRPG.src.Entities
     public class Wizard : Hero
     {
 
-        public Wizard(string name, int level, string heroType) : base(name, level, heroType)
+        public Wizard(string name, int level, string heroType, double life) : base(name, level, heroType, life)
         {
 
         }
 
-        public override string Attack()
+        public override string Attack(string hero, double damage)
         {
             return this.name + " Lançou um ataque mágico!";
+        }
+
+        public string Attack(int Bonus)
+        {
+            if (Bonus > 6)
+            {
+                return this.name + " Lançou um poderoso ataque mágico!\nDano: " + Bonus;
+            }
+            else
+            {
+                return this.name + " Lançou um ataque mágico!\nDano: " + Bonus;
+            }
         }
     }
 }
