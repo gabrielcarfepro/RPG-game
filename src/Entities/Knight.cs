42d00a267eae2ac9c4e090afa06f12cdde575c35
Gabriel Ferreira <gabrielcarfepro@gmail.com>
2022-05-29 13:12:05 -0300

    class Knight created and inherited from the Hero class

---
+++ src/Entities/Knight.cs
@@ -0,0 +1,10 @@
+namespace jogoRPG.src.Entities
+{
+    public class Knight : Hero
+    {
+        public Knight(string name, int level, string heroType) : base(name, level, heroType)
+        {
+
+        }
+    }
+}
