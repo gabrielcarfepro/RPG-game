dd0cc9a9e32132a5f7845ab7662fdf6017001101
Gabriel Ferreira <gabrielcarfepro@gmail.com>
2022-05-29 14:01:49 -0300

    Include the new attribute, named life

--- src/Entities/Knight.cs
+++ src/Entities/Knight.cs
@@ -2,7 +2,7 @@ namespace jogoRPG.src.Entities
 {
     public class Knight : Hero
     {
-        public Knight(string name, int level, string heroType) : base(name, level, heroType)
+        public Knight(string name, int level, string heroType, double life) : base(name, level, heroType, life)
         {
 
         }
