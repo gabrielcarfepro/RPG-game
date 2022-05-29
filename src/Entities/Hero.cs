78e422c7a7467e6267e81f985410608f4330aaa8
Gabriel Ferreira <gabrielcarfepro@gmail.com>
2022-05-29 14:00:44 -0300

    Added a new attribute named life, to mensure the life of character

--- src/Entities/Hero.cs
+++ src/Entities/Hero.cs
@@ -2,26 +2,36 @@ namespace jogoRPG.src.Entities
 {
     public abstract class Hero
     {
+        public int extraLifePercentagePerLevel = 30;
 
-        public Hero(string name, int level, string heroType)
+        public Hero(string name, int level, string heroType, double life)
         {
             this.name = name;
             this.level = level;
             this.heroType = heroType;
+            this.life = life;
         }
 
         public string name;
         public int level;
         public string heroType;
+        public double life;
 
         public override string ToString()
         {
             return $"Características do Personagem\n Nome: {this.name}\n Level: {this.level}\n Classe: {this.heroType}";
         }
 
-        public virtual string Attack()
+        public virtual string Attack(string hero, double damage)
         {
+            this.DamageTaken(damage);
             return this.name + " Atacou com a sua espada!";
+        }
+
+        public string DamageTaken(double damage)
+        {
+            this.life -= damage;
+            return $"{this.name} - vida: {this.life}";
         }
     }
 }
