7befc1a767d3be6c3a6555713346aebb8eb33c08
Gabriel Ferreira <gabrielcarfepro@gmail.com>
2022-05-29 13:59:07 -0300

    Ninja class created and inherited from Hero class

---
+++ src/Entities/Ninja.cs
@@ -0,0 +1,10 @@
+namespace jogoRPG.src.Entities
+{
+    public class Ninja : Hero
+    {
+        public Ninja(string name, int level, string heroType, double life) : base(name, level, heroType, life)
+        {
+
+        }
+    }
+}
